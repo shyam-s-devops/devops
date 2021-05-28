8a53f1f3602c9f07e9cf8ee8afe3a6e57e4f1e0c
ShyamRayasam <sunderrs2004@yahoo.com>
2021-05-28 20:27:31 +0530

    Committing Changes

--- shell/downloadFile.sh
+++ shell/downloadFile.sh
@@ -1,28 +1,39 @@
 DIR="glowroot"
 FILE="glowroot-0.13.6-dist.zip"
+echo "          "
 if [ -d "$DIR" ]; then
   echo "%%%%%%%%%%Remove glowroot folder and all files%%%%%%%%%% \n"
   # Take action if $DIR exists. #
   rm -rvf $DIR
+  echo "          "
 fi
 if [ -f "$FILE" ]; then
   echo "%%%%%%%%%%Remove glowroot zip file%%%%%%%%%% \n"
   # Take action if $FILE exists. #
-  rm $FILE  
+  rm $FILE
+  echo "          "
 fi
 echo "%%%%%%%%%%Downloading Zip File%%%%%%%%%%"
 wget https://github.com/glowroot/glowroot/releases/download/v0.13.6/glowroot-0.13.6-dist.zip
+echo "          "
 echo "%%%%%%%%%%Unzipping zip file%%%%%%%%%%"
 unzip -q glowroot-0.13.6-dist.zip
+echo "          "
 echo "%%%%%%%%%%List contents of Zip file%%%%%%%%%%"
 unzip -l glowroot-0.13.6-dist.zip
+echo "          "
 echo "%%%%%%%%%%List contents in glowroot folder%%%%%%%%%%"
 ls -lhrt glowroot
+echo "          "
 echo "%%%%%%%%%%Removing files with extension .jar in glowroot folder%%%%%%%%%%"
 rm glowroot/*.jar
+echo "          "
 echo "%%%%%%%%%%Listing contents of glowroot folder%%%%%%%%%%"
 ls -lhrt glowroot
+echo "          "
 echo "%%%%%%%%%%Removing files with extension .jar in glowroot/lib folder%%%%%%%%%%"
 rm glowroot/lib/*.jar
+echo "          "
 echo "%%%%%%%%%%Listing contents of glowroot/lib folder%%%%%%%%%%"
 ls -lhrt glowroot/lib
+echo "          "
---
+++ shell/gitCommPractice.sh
@@ -0,0 +1,16 @@
+Login to Github account (if you haven't created, then created a github account first)
+
+$ git config credential.helper store
+$ git push https://github.com/owner/repo.git
+
+Username for 'https://github.com': <shyam.s.devops@gmail.com>
+Password for 'https://USERNAME@github.com': <AnandaDevi@369>
+
+Create a public repository
+Create some sample files
+Commit Changes
+Clone repository on your local lab machine
+make changes to some files locally
+push your changes to github using git push command
+make changes to files on github
+pull these changes to your local repo using git pull command
